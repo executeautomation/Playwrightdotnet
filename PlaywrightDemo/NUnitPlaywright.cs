9b68adbe1abb6b54a7625646576852b70149d691
ExecuteAutomation <kartmcad@gmail.com>
2022-05-08 11:22:59 +1200

    Until lecture 2

---
+++ PlaywrightDemo/NUnitPlaywright.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace PlaywrightDemo;
+
+public class NUnitPlaywright : PageTest
+{
+    [SetUp]
+    public async Task Setup()
+    {
+        await Page.GotoAsync("http://www.eaapp.somee.com");
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        await Page.ClickAsync("text=Login");
+        await Page.FillAsync("#UserName", "admin");
+        await Page.FillAsync("#Password", "password");
+        await Page.ClickAsync("text=Log in");
+        await Expect(Page.Locator("text='Employee Details'")).ToBeVisibleAsync();
+    }
+}
