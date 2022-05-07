43077ac23e0721763442d9f4e39eb7d87d318371
ExecuteAutomation <kartmcad@gmail.com>
2022-05-08 11:23:58 +1200

    WIP

--- PlaywrightDemo/UnitTest1.cs
+++ PlaywrightDemo/UnitTest1.cs
@@ -1,38 +1,38 @@
-// using System.Threading.Tasks;
-// using Microsoft.Playwright;
-// using NUnit.Framework;
-//
-// namespace PlaywrightDemo;
-//
-// public class Tests
-// {
-//     [SetUp]
-//     public void Setup()
-//     {
-//     }
-//
-//     [Test]
-//     public async Task Test1()
-//     {
-//         //Playwright
-//         using var playwright = await Playwright.CreateAsync();
-//         //Browser
-//         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-//         {
-//             Headless = false
-//         });
-//         //Page
-//         var page = await browser.NewPageAsync();
-//         await page.GotoAsync("http://www.eaapp.somee.com");
-//         await page.ClickAsync("text=Login");
-//         await page.ScreenshotAsync(new PageScreenshotOptions
-//         {
-//             Path = "EAApp.jpg"
-//         });
-//         await page.FillAsync("#UserName", "admin");
-//         await page.FillAsync("#Password", "password");
-//         await page.ClickAsync("text=Log in");
-//         var isExist = await page.Locator("text='Employee Details'").IsVisibleAsync();
-//         Assert.IsTrue(isExist);
-//     }
-// }
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace PlaywrightDemo;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        //Playwright
+        using var playwright = await Playwright.CreateAsync();
+        //Browser
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false
+        });
+        //Page
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("http://www.eaapp.somee.com");
+        await page.ClickAsync("text=Login");
+        await page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "EAApp.jpg"
+        });
+        await page.FillAsync("#UserName", "admin");
+        await page.FillAsync("#Password", "password");
+        await page.ClickAsync("text=Log in");
+        var isExist = await page.Locator("text='Employee Details'").IsVisibleAsync();
+        Assert.IsTrue(isExist);
+    }
+}
