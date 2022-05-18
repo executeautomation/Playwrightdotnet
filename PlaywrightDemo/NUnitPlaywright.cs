761e791d571fef9a706e9a7d8eff87991f84773e
ExecuteAutomation <kkrishnan@everydayhealthgroup.com>
2022-05-18 16:01:22 +1200

    Until Locators

--- PlaywrightDemo/NUnitPlaywright.cs
+++ PlaywrightDemo/NUnitPlaywright.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
 
@@ -15,10 +16,16 @@ public class NUnitPlaywright : PageTest
     [Test]
     public async Task Test1()
     {
+        //Using Locators
+        var lnkLogin = Page.Locator("text=Login");
+        await lnkLogin.ClickAsync();
         await Page.ClickAsync("text=Login");
         await Page.FillAsync("#UserName", "admin");
         await Page.FillAsync("#Password", "password");
-        await Page.ClickAsync("text=Log in");
+        //Using Locator with Page Locator Options
+        var btnLogin = Page.Locator("input", new PageLocatorOptions { HasTextString = "Log in" });
+        await btnLogin.ClickAsync();
+        //await Page.ClickAsync("text=Log in");
         await Expect(Page.Locator("text='Employee Details'")).ToBeVisibleAsync();
     }
 }
