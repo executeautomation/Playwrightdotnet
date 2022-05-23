b44c0a3a104ffc7e13d78d8ad44b5ef4f65d019a
ExecuteAutomation <kkrishnan@everydayhealthgroup.com>
2022-05-23 18:17:43 +1200

    Renamed UnitTest1.cs

--- PlaywrightDemo/PlaywrightBasics.cs
+++ PlaywrightDemo/PlaywrightBasics.cs
@@ -39,7 +39,7 @@ public class Tests
         var isExist = await page.Locator("text='Employee Details'").IsVisibleAsync();
         Assert.IsTrue(isExist);
     }
-    
+
     [Test]
     public async Task TestWithPOM()
     {
@@ -61,25 +61,28 @@ public class Tests
         Assert.IsTrue(isExist);
     }
 
-    
+
     [Test]
     public async Task WaitTest()
     {
         using var playwright = await Playwright.CreateAsync();
         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
-            Headless = false,
+            Headless = false
         });
         var context = await browser.NewContextAsync();
         var page = await context.NewPageAsync();
         await page.GotoAsync("https://demos.telerik.com/kendo-ui/window/angular");
-        await page.Locator("text=Calendar May 2022SuMoTuWeThFrSa1234567891011121314151617181920212223242526272829 >> [aria-label=\"Close\"]").ClickAsync();
-        await page.Locator("text=Calendar May 2022SuMoTuWeThFrSa1234567891011121314151617181920212223242526272829 >> [aria-label=\"Close\"]").ClickAsync();
+        await page.Locator(
+                "text=Calendar May 2022SuMoTuWeThFrSa1234567891011121314151617181920212223242526272829 >> [aria-label=\"Close\"]")
+            .ClickAsync();
+        await page.Locator(
+                "text=Calendar May 2022SuMoTuWeThFrSa1234567891011121314151617181920212223242526272829 >> [aria-label=\"Close\"]")
+            .ClickAsync();
         // Click button:has-text("Open AJAX content")
         await page.Locator("button:has-text(\"Open AJAX content\")").ClickAsync();
-  
     }
-    
+
     [Test]
     public async Task TestNetwork()
     {
@@ -104,12 +107,10 @@ public class Tests
         await loginPage.ClickEmployeeList();
         //give the response details
         var getResponse = await waitResponse;
-        
+
         //Way 2 - Wait for response
-        var response = await page.RunAndWaitForResponseAsync(async () =>
-        {
-            await loginPage.ClickEmployeeList();
-        }, x => x.Url.Contains("/Employee") && x.Status == 200);
+        var response = await page.RunAndWaitForResponseAsync(async () => { await loginPage.ClickEmployeeList(); },
+            x => x.Url.Contains("/Employee") && x.Status == 200);
 
         var isExist = await loginPage.IsEmployeeDetailsExists();
         Assert.IsTrue(isExist);
@@ -135,16 +136,13 @@ public class Tests
         {
             HasTextString = "Login"
         }).ClickAsync();
-        
-        var request = await page.RunAndWaitForRequestAsync(async () =>
-        {
-            await page.Locator("text=✕").ClickAsync();
-        }, x => x.Url.Contains("flipkart.d1.sc.omtrdc.net") && x.Method == "GET");
+
+        var request = await page.RunAndWaitForRequestAsync(async () => { await page.Locator("text=✕").ClickAsync(); },
+            x => x.Url.Contains("flipkart.d1.sc.omtrdc.net") && x.Method == "GET");
 
         var returnData = HttpUtility.UrlDecode(request.Url);
 
         returnData.Should().Contain("Account Login:Displayed Exit");
-
     }
 
     [Test]
@@ -168,11 +166,10 @@ public class Tests
             else
                 await route.ContinueAsync();
         });
-        
+
         await page.GotoAsync("https://www.flipkart.com/", new PageGotoOptions
         {
             WaitUntil = WaitUntilState.NetworkIdle
         });
-
     }
 }
