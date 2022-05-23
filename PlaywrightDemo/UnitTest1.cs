e0daf344d372ff69a3b2cd8c56d2334d0b4ff5c4
ExecuteAutomation <kkrishnan@everydayhealthgroup.com>
2022-05-23 15:42:22 +1200

    Until Network Interception

--- PlaywrightDemo/UnitTest1.cs
+++ PlaywrightDemo/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using System.Web;
 using FluentAssertions;
@@ -143,6 +144,35 @@ public class Tests
         var returnData = HttpUtility.UrlDecode(request.Url);
 
         returnData.Should().Contain("Account Login:Displayed Exit");
+
+    }
+
+    [Test]
+    public async Task? FlipkartNetworkInterception()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false
+        });
+        var context = await browser.NewContextAsync();
+        var page = await context.NewPageAsync();
+
+        page.Request += (_, request) => Console.WriteLine(request.Method + "---" + request.Url);
+        page.Response += (_, response) => Console.WriteLine(response.Status + "---" + response.Url);
+
+        await page.RouteAsync("**/*", async route =>
+        {
+            if (route.Request.ResourceType == "image")
+                await route.AbortAsync();
+            else
+                await route.ContinueAsync();
+        });
+        
+        await page.GotoAsync("https://www.flipkart.com/", new PageGotoOptions
+        {
+            WaitUntil = WaitUntilState.NetworkIdle
+        });
 
     }
 }
