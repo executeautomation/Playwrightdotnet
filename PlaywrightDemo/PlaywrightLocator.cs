de4665c3ba44f32ce6ab9d510abc22d12208df75
ExecuteAutomation <karthik@techgeek.co.in>
2023-04-14 20:28:44 +1200

    Added new ARIA locator support

--- PlaywrightDemo/PlaywrightLocator.cs
+++ PlaywrightDemo/PlaywrightLocator.cs
@@ -64,4 +64,93 @@ public class PlaywrightLocator
             FullPage = true
         });
     }
+
+    [Test]
+    public async Task LocatorTestForEA()
+    {
+ using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false,
+        });
+        var context = await browser.NewContextAsync();
+
+        var page = await context.NewPageAsync();
+
+        await page.GotoAsync("https://executeautomation.com/");
+
+        await page.GetByRole(AriaRole.Link, new() { Name = "Sign In" }).ClickAsync();
+
+        await page.GetByRole(AriaRole.Link, new() { Name = "Sign Up" }).ClickAsync();
+        
+        await page.GetByPlaceholder("First Name").FillAsync("DemoUser1");
+
+        await page.GetByPlaceholder("Last Name").FillAsync("KK");
+
+        await page.Locator("#react-select-2-input").FillAsync("New");
+
+        await page.GetByText("New Zealand", new() { Exact = true }).ClickAsync();
+
+        await page.GetByPlaceholder("Email Address").FillAsync("demouser1@demo1.com");
+
+        await page.GetByPlaceholder("Password", new() { Exact = true }).FillAsync("password");
+
+        await page.GetByPlaceholder("Enter Password Again").FillAsync("password");
+
+        await page.GetByPlaceholder("Profession").FillAsync("Tester");
+
+        await page.GetByRole(AriaRole.Button, new() { Name = "Create Account" }).ClickAsync();
+
+        await page.Locator("img:nth-child(2)").ClickAsync();
+
+        await page.GetByRole(AriaRole.Button, new() { Name = "Create Account" }).ClickAsync();
+
+        await page.Locator(".ct-logo-header").ClickAsync();
+
+        await page.GetByRole(AriaRole.Link, new() { Name = "Codeless automation Lean" }).ClickAsync();
+
+        await page.GetByRole(AriaRole.Link, new() { Name = "1 Katalon Studio" }).ClickAsync();
+
+        await page.GetByRole(AriaRole.Button, new() { Name = "Watch" }).ClickAsync();
+
+        await page.GetByRole(AriaRole.Heading, new() { Name = "Part 2 - Record & playback," }).ClickAsync();
+
+        await page.GetByRole(AriaRole.Button, new() { Name = "Close" }).ClickAsync();
+    }
+
+
+    [Test]
+    public async Task LocatorTestsForEA2()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false,
+        });
+        var context = await browser.NewContextAsync();
+
+        var page = await context.NewPageAsync();
+
+        await page.GotoAsync("http://localhost:8000/Product/Create");
+
+        var form1 = page.GetByRole(AriaRole.Form, new() { Name = "form1" });
+        var form2 = page.GetByRole(AriaRole.Form, new() { Name = "form2" });
+
+        await form1.GetByLabel("Name").FillAsync("keyboard");
+
+        await form1.GetByLabel("Description").FillAsync("gaming keyboard");
+
+        await form1.GetByRole(AriaRole.Spinbutton).FillAsync("20");
+
+        await form1.Locator("#ProductType").SelectOptionAsync(new[] { "1" });
+
+        await form2.Locator("#Name").FillAsync("Mouse");
+
+        await form2.Locator("#Description").FillAsync("Gaming mouse");
+
+        await form2.GetByRole(AriaRole.Spinbutton).FillAsync("10");
+
+        await form2.Locator("#ProductType").SelectOptionAsync(new[] { "1" });
+
+    }
 }
